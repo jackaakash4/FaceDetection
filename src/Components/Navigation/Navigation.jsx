2e1114f551f3a0b804e6678540600d50785e912a
jackaakash4 <jackaakash4@gmail.com>
2023-09-23 12:26:37 +0545

    Css changed

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -4,7 +4,7 @@ import './Navigation.css'
 function Navigation({onRouteChange}) {
   return (
         <nav className='navigation'>
-            <p className='f3 link dim black underline pa3 pointer' onClick={()=>onRouteChange('signin')}>SignOut</p>
+            <p className='f3 link dim black underline pa3 pointer white' onClick={()=>onRouteChange('signin')}>SignOut</p>
         </nav> 
     )
 }
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -7,22 +7,22 @@ function Register({onRouteChange}) {
     <main class="pa4 black-80">
         <form class="measure">
             <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-                <legend class="f1 fw6 ph0 mh0">Register</legend>
+                <legend class="f1 fw6 ph0 mh0 white">Register</legend>
                 <div class="mt3">
-                    <label class="db fw6 lh-copy f6" for="name">Name</label>
+                    <label class="db fw6 lh-copy f6 white" htmlFor="name">Name</label>
                     <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
                 </div>
                 <div class="mt3">
-                    <label class="db fw6 lh-copy f6" for="email-address">Email</label>
+                    <label class="db fw6 lh-copy f6 white" htmlFor="email-address">Email</label>
                     <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
                 </div>
                 <div class="mv3">
-                    <label class="db fw6 lh-copy f6" for="password">Password</label>
+                    <label class="db fw6 lh-copy f6 white" htmlFor="password">Password</label>
                     <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
                 </div>
             </fieldset>
     <div class="">
-      <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" onClick={()=>onRouteChange('signin')}/>
+      <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib white" type="submit" value="Register" onClick={()=>onRouteChange('signin')}/>
     </div>
     
   </form>
